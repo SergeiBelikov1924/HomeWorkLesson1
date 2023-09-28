a345751d175070a9bdfd45e35c55cfd42c7b9a6a
Кот <Sergei Belikov>
2023-09-28 18:52:31 +0700

    task04

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
+
 Console.WriteLine("Введите первое число: ");
 int firstNumber = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
